f2b44d3448dd39e60e5ce139a782ac39b9f11e67
simonelupone <simone.lupone@gmail.com>
2025-04-22 17:02:57 +0200

    feat: add show endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -8,4 +8,22 @@ router.get('/', (req, res) => {
     res.json(posts)
 })
 
+// show
+router.get('/:id', (req, res) => {
+    const postId = parseInt(req.params.id)
+
+    const post = posts.find(post => post.id === postId)
+
+    if(!post){
+        res.status(404)
+
+        res.json({
+            error: 'Not Found',
+            message: 'Post not found'
+        })
+    }
+
+    res.json(post)
+})
+
 module.exports = router
